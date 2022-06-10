86b43969711373e1315bc79a25f7aa91ce119ed9
Aditya925 <adityarahalkar925@gmail.com>
2022-06-10 14:05:16 +0530

    changed print statement

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -6,13 +6,14 @@
 pub fn scalartypes(){
     println!("          
                          INTEGER TYPES
-               LENGTH	   SIGNED	  UNSIGNED
-                8-bit        i8       u8
-                16-bit	     i16	    u16
-                32-bit	     i32	    u32
-                64-bit	     i64	    u64
-                128-bit	     i128	    u128
-                arch	     isize	    usize      ");
+               LENGTH	     SIGNED	   UNSIGNED
+                8-bit        i8         u8
+                16-bit	     i16	      u16
+                32-bit	     i32	      u32
+                64-bit	     i64	      u64
+                128-bit	     i128	      u128
+                arch	       isize	    usize      
+                                                   ");
 
     println!("           
                         INTEGER LITERALS
@@ -21,7 +22,8 @@ pub fn scalartypes(){
                  Hex	                0xff
                  Octal	              0o77
                  Binary	              0b1111_0000
-                 Byte(u8 only)	      b'A'         ");
+                 Byte(u8 only)	      b'A'         
+                                                   ");
 
     let x: f64 = 3.2;
     let y: f32 = 2.2;
