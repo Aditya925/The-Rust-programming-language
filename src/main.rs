fa752c07bd98140db57404ffea19fcc5f157f872
Aditya925 <adityarahalkar925@gmail.com>
2022-06-11 21:57:37 +0530

    New file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 mod chapter_1;
 mod chapter_2;
+mod chapter_3;
+
 fn main(){
     chapter_1::print();
     chapter_1::run();
     chapter_1::constant();
-    chapter_1::shadowing();
-    chapter_2::scalartypes();
+    chapter_1::shadowing();        chapter_2::scalartypes();
     chapter_2::numeric_operations();
-    chapter_2::tuple();
-    chapter_2::array();
+    chapter_2::tuple();        chapter_2::array();
+    chapter_3::function();
+    chapter_3::parameters();
+    chapter_3::display();
+    chapter_3::no_parameter();
+    chapter_3::with_parameter();
 }
