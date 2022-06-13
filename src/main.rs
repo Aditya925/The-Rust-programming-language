7a71f2016ca72ebdafa9fdcaf9d73d8dda0ae470
Aditya925 <adityarahalkar925@gmail.com>
2022-06-13 20:19:47 +0530

    Change

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@ mod chapter_2;
 mod chapter_3;
 
 fn main(){
-    chapter_1::print();
+  /*chapter_1::print();
     chapter_1::run();
     chapter_1::constant();
-    chapter_1::shadowing();        chapter_2::scalartypes();
+    chapter_1::shadowing();        
+    chapter_2::scalartypes();
     chapter_2::numeric_operations();
-    chapter_2::tuple();        chapter_2::array();
+    chapter_2::tuple();        
+    chapter_2::array();
     chapter_2::function();
     chapter_2::parameters();
     chapter_2::display();
     chapter_2::no_parameter();
     chapter_2::with_parameter();
+    chapter_3::conditions();
+    chapter_3::if_in_let();
+    chapter_3::simple_loop();
+    chapter_3::returning_values_from_loops();
+    chapter_3::while_loop();
+    chapter_3::for_loop();*/
 }
