991e8ab1ae00984b2fa6b09488f86bfec54663e5
Aditya925 <adityarahalkar925@gmail.com>
2022-07-02 19:36:01 +0530

    changes

--- src/chapter_4_ownership.rs
+++ src/chapter_4_ownership.rs
@@ -20,7 +20,7 @@ pub fn run(){
 
     let s1 = String::from("hello");
     let s2 = s1;
- // println!("s2 is: {}",s2);     -----> This statement produces error value of s1 is already moved to s2
+ // println!("s1 is: {}",s1);     -----> This statement produces error value of s1 is already moved to s2
     println!("s2 is: {}",s2);
 }
 
